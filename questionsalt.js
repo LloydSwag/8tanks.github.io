92ef2670feebceae3ba22b1f280b8039cd33d160
Lloyd <32413292+LloydSwag@users.noreply.github.com>
2021-02-14 16:23:11 +0100

    Add files via upload

---
+++ questionsalt.js
@@ -0,0 +1,137 @@
+questions = [
+    {
+        "question": "I support societal development.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Ancient Egyptian infrastructure looks the best within the Ancient World.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Agriculture is more important than business development.",
+        "effect": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": 0,
+            "scty": -3
+        }
+    },
+    {
+        "question": "I go with the flow and follow what other people do.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I support rapid industrialization.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Strength is more important than persuasion.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 8
+        }
+    },
+    {
+        "question": "The ultimate form of power comes from strength and combat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Clay and Concrete are vital materials that led to modern infrastrucuture.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Due to the fact that Sand can potentially hurt people's eyes with wind, they should be kept away from children.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Walls around borders are important for self-defence and reducing criminality.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Sandy Deserts are better than Mesa Deserts.",
+        "effect": {
+            "econ": 5,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The government should regulate more in public beach areas.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Nothingness is what we should all be more afraid of.",
+        "effect": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": 2,
+            "scty": 5
+        }
+    },
+    {
+        "question": "A void is not as chaotic and bad as many people think.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Silt is more important than either Sand or Clay.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    }
+];
