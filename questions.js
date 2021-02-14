9aec66dbc1708843f09655c5f3c42bdc885eb639
Lloyd <32413292+LloydSwag@users.noreply.github.com>
2021-02-15 00:04:51 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": "Tracks are superior to wheels.",
+        "question": "Tracks are superior to wheels.", //Transmission
         "effect": {
             "econ": 5,
             "dipl": 0,
@@ -9,7 +9,7 @@ questions = [
         }
     },
     {
-        "question": "Tracks are more versatile than wheels.",
+        "question": "Tracks are more versatile than wheels.", //Transmission
         "effect": {
             "econ": 5,
             "dipl": 0,
@@ -18,7 +18,7 @@ questions = [
         }
     },
     {
-        "question": "Heavy tanks are a waste of resources.",
+        "question": "Heavy tanks are a waste of resources.", //Classification
         "effect": {
             "econ": 0,
             "dipl": 5,
@@ -27,7 +27,7 @@ questions = [
         }
     },
     {
-        "question": "Tank guns are more useful towards other vehicles.",
+        "question": "Tank guns are more useful towards other vehicles.", //Weapon
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -36,7 +36,7 @@ questions = [
         }
     },
     {
-        "question": "Wheels are easier to maintain than tracks.",
+        "question": "Wheels are easier to maintain than tracks.", //Transmission
         "effect": {
             "econ": -5,
             "dipl": 0,
@@ -45,7 +45,7 @@ questions = [
         }
     },
     {
-        "question": "Older tanks look better than modern ones.",
+        "question": "Older tanks look better than modern ones.", //Time
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -54,25 +54,25 @@ questions = [
         }
     },
     {
-        "question": "Tiny powder or particles fall down when I pick it up.",
+        "question": "Wheels are better for urban environment.", //Transmission
         "effect": {
-            "econ": 5,
+            "econ": -5,
             "dipl": 0,
-            "govt": 5,
-            "scty": -5
+            "govt": 0,
+            "scty": 0
         }
     },
     {
-        "question": "Modern tanks are objectively better than older ones.",
+        "question": "Modern tanks are objectively better than older ones.", //Time
         "effect": {
             "econ": 0,
             "dipl": 0,
             "govt": 0,
-            "scty": -5
+            "scty": 5
         }
     },
     {
-        "question": "It is easier to clear a building with a machinegun than a artillery piece.",
+        "question": "It is easier to take down infantry with small arms than artillery.", //Weapon
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -81,7 +81,7 @@ questions = [
         }
     },
     {
-        "question": "Light tanks have proven to be far better than heavy tanks.",
+        "question": "Light tanks have proven to be far better than heavy tanks.", //Classification
         "effect": {
             "econ": 0,
             "dipl": 5,
@@ -90,56 +90,56 @@ questions = [
         }
     },
     {
-        "question": "The more guns, the better.",
+        "question": "The more guns, the better.", //Weapon
         "effect": {
             "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "The more turrets, the better.", //Classification
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Flame tanks are a good idea.", //Weapon
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Landcruisers are a viable and realistic concept.", //Classification
+        "effect": {
+            "econ": 5,
             "dipl": -5,
             "govt": 0,
-            "scty": 5
-        }
-    },
-    {
-        "question": "Putting it in a cup of water makes it dissolve.",
-        "effect": {
-            "econ": 0,
-            "dipl": 5,
-            "govt": 5,
-            "scty": -5
-        }
-    },
-    {
-        "question": "Adding water to it makes it change color.",
-        "effect": {
-            "econ": 5,
-            "dipl": 5,
-            "govt": 0,
-            "scty": -5
-        }
-    },
-    {
-        "question": "The material looks like or is a building material.",
-        "effect": {
-            "econ": 0,
-            "dipl": 5,
-            "govt": 0,
-            "scty": 5
-        }
-    },
-    {
-        "question": "The material is dust-like.",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 5,
             "scty": 0
         }
     },
     {
-        "question": "The material originates from rocks.",
+        "question": "There are many design choices in older tanks that we should use today.", //Time
         "effect": {
-            "econ": 5,
+            "econ": 0,
             "dipl": 0,
-            "govt": 5,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "We could use more wheels in todays vehicle designs.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
             "scty": 0
         }
     },
