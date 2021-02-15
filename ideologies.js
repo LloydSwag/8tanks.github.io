d9f8ed72d0e4a14dadf78e2a3f4158aa77f9e2c1
Lloyd <32413292+LloydSwag@users.noreply.github.com>
2021-02-15 12:04:51 +0100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -21,25 +21,25 @@ ideologies = [
     {
         "name": "Tankette",
         "stats": {
-            "econ": 0,
+            "econ": 61,
             "dipl": 20,
-            "govt": 10,
+            "govt": 0,
             "scty": 0
         }
     },
     {
         "name": "Light Tank",
         "stats": {
-            "econ": 0,
+            "econ": 61,
             "dipl": 20,
-            "govt": 50,
+            "govt": 0,
             "scty": 0
         }
     },
     {
         "name": "Medium Tank",
         "stats": {
-            "econ": 30,
+            "econ": 61,
             "dipl": 40,
             "govt": 0,
             "scty": 0
@@ -48,7 +48,7 @@ ideologies = [
     {
         "name": "Heavy Tank",
         "stats": {
-            "econ": 0,
+            "econ": 61,
             "dipl": 61,
             "govt": 0,
             "scty": 0
@@ -57,7 +57,7 @@ ideologies = [
     {
         "name": "Monster",
         "stats": {
-            "econ": 0,
+            "econ": 61,
             "dipl": 91,
             "govt": 0,
             "scty": 0
@@ -66,7 +66,7 @@ ideologies = [
     {
         "name": "Super Heavy",
         "stats": {
-            "econ": 0,
+            "econ": 61,
             "dipl": 76,
             "govt": 0,
             "scty": 0
@@ -75,7 +75,7 @@ ideologies = [
     {
         "name": "Landship",
         "stats": {
-            "econ": 51,
+            "econ": 61,
             "dipl": 76,
             "govt": 0,
             "scty": 24
@@ -102,7 +102,7 @@ ideologies = [
     {
         "name": "Monster",
         "stats": {
-            "econ": 20,
+            "econ": 61,
             "dipl": 100,
             "govt": 90,
             "scty": 0
